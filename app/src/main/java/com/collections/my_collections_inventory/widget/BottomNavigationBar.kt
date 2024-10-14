006e6fa63861a4c2e5b72da07d2514f5fe642c90
Korentin <korentin.kolaczek@gmail.com>
2024-10-14 14:38:30 +0200

    (fix) : navbar color and rounded box

--- app/src/main/java/com/collections/my_collections_inventory/widget/BottomNavigationBar.kt
+++ app/src/main/java/com/collections/my_collections_inventory/widget/BottomNavigationBar.kt
@@ -1,17 +1,16 @@
+import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 
 @Composable
 fun BottomNavigationBar(navController: NavController, selectedIndex: Int, onItemSelected: (Int) -> Unit) {
-    NavigationBar {
+    NavigationBar(containerColor = Color(0xFFAEC5EB)) {
         BottomNavigationItem().bottomNavigationItems().forEachIndexed { index, navigationItem ->
             NavigationBarItem(
                 selected = index == selectedIndex,
-                label = { Text(navigationItem.label) },
                 icon = {
                     Icon(
                         navigationItem.icon,
