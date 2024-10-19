0eed97b90e9038e2a07ad3c9d82cfee26620030f
EdouardHoest <hoest.edouard@gmail.com>
2024-10-19 21:40:30 +0200

    (fix): remove nav bar from login pages

--- app/src/main/java/com/collections/my_collections_inventory/screen/LoginScreen.kt
+++ app/src/main/java/com/collections/my_collections_inventory/screen/LoginScreen.kt
@@ -1,6 +1,5 @@
 package com.collections.my_collections_inventory.screen
 
-import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
